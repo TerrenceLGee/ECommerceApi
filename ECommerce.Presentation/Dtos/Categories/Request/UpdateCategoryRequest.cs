c5541f9610369d7e9ab48c985a56a760039e622a
terrence <mrgee1978@proton.me>
2025-09-12 21:44:38 -0400

    Twenty-third commit took care of categories UI

--- ECommerce.Presentation/Dtos/Categories/Request/UpdateCategoryRequest.cs
+++ ECommerce.Presentation/Dtos/Categories/Request/UpdateCategoryRequest.cs
@@ -6,4 +6,5 @@ public class UpdateCategoryRequest
 {
     [Required] [MaxLength(100)] public string Name { get; set; } = string.Empty;
     [MaxLength(500)] public string? Description { get; set; }
+    [Required] public bool IsActive { get; set; }
 }
--- ECommerce.Presentation/UI/Operations/Categories/CategoriesUI.cs
+++ ECommerce.Presentation/UI/Operations/Categories/CategoriesUI.cs
@@ -1,3 +1,5 @@
+using ECommerce.Presentation.Common.Results;
+using ECommerce.Presentation.Dtos.Categories.Request;
 using ECommerce.Presentation.Dtos.Categories.Response;
 using ECommerce.Presentation.Dtos.Products.Response;
 using ECommerce.Presentation.Interfaces;
@@ -65,6 +67,159 @@ public class CategoriesUI
         }
     }
 
+    public async Task HandleAddCategory()
+    {
+        AnsiConsole.MarkupLine("[bold underline yellow]Add a category to the database[/]");
+        AnsiConsole.WriteLine();
+
+        var name = AnsiConsole.Ask<string>("Enter category name: ");
+        var description = AnsiConsole.Ask<string>("Enter category description (optional): ");
+
+        var request = new CreateCategoryRequest
+        {
+            Name = name,
+            Description = description
+        };
+
+        Result<CategoryResponse?> categoryResponseResult = null!;
+
+        await AnsiConsole.Status().StartAsync("Adding category...", async _ =>
+        {
+            categoryResponseResult = await _categoryApiService.CreateCategoryAsync(request);
+        });
+
+        if (categoryResponseResult.IsSuccess && categoryResponseResult.Value is not null)
+        {
+            var categoryResponse = categoryResponseResult.Value;
+            
+            AnsiConsole.MarkupLine("[bold green]Category added successfully![/]");
+            var title = "Newly added category";
+            await DisplayCategoryResponse(categoryResponse, title);
+        }
+        else
+        {
+            AnsiConsole.MarkupLine($"[bold red]Failed to add category to the database: {categoryResponseResult.ErrorMessage}[/]");
+        }
+    }
+
+    public async Task HandleUpdateCategory()
+    {
+        AnsiConsole.MarkupLine("[bold underline yellow]Update an existing category in the database[/]");
+        AnsiConsole.WriteLine();
+
+        AnsiConsole.MarkupLine("[bold underline green]Please choose from an existing category[/]");
+        await HandleViewAllCategories();
+        AnsiConsole.WriteLine();
+
+        var categoryId = AnsiConsole.Ask<int>("Enter the id of the category to update: ");
+
+        var categoryToUpdateResult = await _categoryApiService.GetCategoryByIdAsync(categoryId);
+
+        if (categoryToUpdateResult.IsFailure || categoryToUpdateResult.Value is null)
+        {
+            AnsiConsole.MarkupLine($"{categoryToUpdateResult.ErrorMessage}");
+            AnsiConsole.MarkupLine("Press any key to continue");
+            Console.ReadKey();
+            AnsiConsole.Clear();
+            return;
+        }
+
+        var categoryToUpdate = categoryToUpdateResult.Value;
+
+        var name = AnsiConsole.Confirm("Update category name? ")
+            ? AnsiConsole.Ask<string>("Enter updated name: ")
+            : categoryToUpdate.Name;
+
+        var description = AnsiConsole.Confirm("Update category description? ")
+            ? AnsiConsole.Ask<string>("Enter updated description: ")
+            : categoryToUpdate.Description;
+
+        var isActive =
+            AnsiConsole.Confirm($"Active status is currently {categoryToUpdate.IsActive}\nUpdate category status? ")
+                ? AnsiConsole.Ask<bool>("Enter updated active status (true/false): ")
+                : categoryToUpdate.IsActive;
+
+        var request = new UpdateCategoryRequest
+        {
+            Name = name,
+            Description = description,
+            IsActive = isActive,
+        };
+
+        Result<CategoryResponse?> categoryResponseResult = null!;
+
+        await AnsiConsole.Status().StartAsync("Updating category...", async _ =>
+        {
+            categoryResponseResult = await _categoryApiService.UpdateCategoryAsync(categoryId, request);
+        });
+
+        if (categoryResponseResult.IsSuccess && categoryResponseResult.Value is not null)
+        {
+            var categoryResponse = categoryResponseResult.Value;
+
+            AnsiConsole.MarkupLine("[bold green]Category updated successfully![/]");
+            var title = "Updated category";
+            await DisplayCategoryResponse(categoryResponse, title);
+        }
+        else
+        {
+            AnsiConsole.MarkupLine($"[red]Failed to update category in the database: {categoryResponseResult.ErrorMessage}[/]");
+        }
+    }
+
+    public async Task HandleDeleteCategory()
+    {
+        AnsiConsole.MarkupLine("[bold underline yellow]Delete an existing category from the database[/]");
+        AnsiConsole.WriteLine();
+
+        AnsiConsole.MarkupLine("[bold underline green]Please choose from an existing category: [/]");
+        await HandleViewAllCategories();
+        AnsiConsole.WriteLine();
+
+        var categoryId = AnsiConsole.Ask<int>("Enter id of category to delete: ");
+
+        var categoryToDeleteResult = await _categoryApiService.GetCategoryByIdAsync(categoryId);
+
+        if (categoryToDeleteResult.IsFailure || categoryToDeleteResult.Value is null)
+        {
+            AnsiConsole.MarkupLine($"[red]{categoryToDeleteResult.ErrorMessage}[/]");
+            AnsiConsole.MarkupLine("Press any key to continue");
+            Console.ReadKey();
+            return;
+        }
+
+        var categoryToDelete = categoryToDeleteResult.Value;
+
+        var confirmDeleteCategory = AnsiConsole.Confirm($"Are you sure you wish to delete {categoryToDelete.Name}? ");
+
+        if (!confirmDeleteCategory)
+        {
+            AnsiConsole.MarkupLine($"[bold red]Category {categoryToDelete.Name} will NOT be deleted[/]");
+            AnsiConsole.WriteLine("Press any key to continue");
+            Console.ReadKey();
+            AnsiConsole.Clear();
+            return;
+        }
+
+        Result<CategoryResponse?> categoryResponseResult = null!;
+
+        await AnsiConsole.Status().StartAsync("Deleting category...", async _ =>
+        {
+            categoryResponseResult = await _categoryApiService.DeleteCategoryAsync(categoryId);
+        });
+
+        if (categoryResponseResult.IsSuccess && categoryResponseResult.Value is not null)
+        {
+            var categoryResponse = categoryResponseResult.Value;
+            AnsiConsole.MarkupLine("[bold green]Category deleted successfully![/]");
+            var title = "Deleted category";
+            await DisplayCategoryResponse(categoryResponse, title);
+        }
+        else
+        {
+            AnsiConsole.MarkupLine($"[bold red]{categoryResponseResult.ErrorMessage}[/]");
+        }
+    }
     private void DisplayCategories(List<CategoryResponse> categories)
     {
         if (categories.Count == 0)
